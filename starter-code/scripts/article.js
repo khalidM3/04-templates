0413c4de062bc71e782b5d00d63806e68b08d649
Khalid Mohamud <kmohamud1@outlook.com>
2017-01-05 13:12:47 -0800

    first half

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,10 +12,13 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#articles').html();
+  var render = Handlebars.compile(source);
+  var context = articles;
+  return render(this);
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
